d01ecd2bc34ba5b1aae4e9a0b532fd6fea48dc53
ZenVega <50147356+ZenVega@users.noreply.github.com>
2020-06-04 15:27:40 +0200

    new logo

--- ressources/burger.js
+++ ressources/burger.js
@@ -1,15 +1,27 @@
 const navSlide = () => {
   const burger = document.querySelector('.burger');
-  const logo = document.querySelector('.logo');
   const nav = document.querySelector('.nav-links');
   const navLinks = document.querySelectorAll('.nav-links li');
+
+  const logoP2 = document.getElementById('two');
+  const logoP3 = document.getElementById('three');
+  const logoP4 = document.getElementById('four');
+
+  let isBurgerOpen = false;
       
   burger.addEventListener('click', () => {
       //logo moves
-      if(logo.style.animation === '0.5s ease 0s 1 normal forwards running moveOut'){
-        logo.style.animation = `moveBack 0.5s ease forwards`;
-      } else if(!logo.style.animation || logo.style.animation == '0.5s ease 0s 1 normal forwards running moveBack'){
-        logo.style.animation = `moveOut 0.5s ease forwards`;
+      if(isBurgerOpen){
+        logoP2.style.animation = `moveBack 0.5s ease-in forwards`;  
+        logoP4.style.animation = `moveUp 0.5s ease-in 0.3s forwards`;
+        logoP3.style.animation = `fadeIn 0.3s ease-in 0.5s forwards`;
+        isBurgerOpen = false;
+
+      } else if(!isBurgerOpen){
+        logoP3.style.animation = `fadeOut 0.3s ease-in forwards`;
+        logoP2.style.animation = `moveOut 0.5s ease-in 0.3s forwards`;
+        logoP4.style.animation = `moveDown 0.5s ease-in 0.5s forwards`;
+        isBurgerOpen = true;
       }
       //Toggle nav
     nav.classList.toggle('nav-active');
