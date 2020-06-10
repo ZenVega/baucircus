4f12c9a282d4abd0feb416632927e6edd8c4a9f0
ZenVega <50147356+ZenVega@users.noreply.github.com>
2020-06-10 19:06:33 +0200

    flow

--- ressources/burger.js
+++ ressources/burger.js
@@ -1,29 +1,11 @@
 const navSlide = () => {
   const burger = document.querySelector('.burger');
-  const nav = document.querySelector('.nav-links');
+  const nav = document.querySelector('nav');
   const navLinks = document.querySelectorAll('.nav-links li');
-
-  const logoP2 = document.getElementById('two');
-  const logoP3 = document.getElementById('three');
-  const logoP4 = document.getElementById('four');
-
-  let isBurgerOpen = false;
       
   burger.addEventListener('click', () => {
-      //logo moves
-      if(isBurgerOpen){
-        logoP2.style.animation = `moveBack 0.5s ease-in forwards`;  
-        logoP4.style.animation = `moveUp 0.5s ease-in 0.3s forwards`;
-        logoP3.style.animation = `fadeIn 0.8s ease 0.5s forwards`;
-        isBurgerOpen = false;
 
-      } else if(!isBurgerOpen){
-        logoP3.style.animation = `fadeOut 0.3s ease-in forwards`;
-        logoP2.style.animation = `moveOut 0.5s ease-in 0.3s forwards`;
-        logoP4.style.animation = `moveDown 0.5s ease-in 0.5s forwards`;
-        isBurgerOpen = true;
-      }
-      //Toggle nav
+      // //Toggle nav
     nav.classList.toggle('nav-active');
       //animation
     navLinks.forEach((link, index) => {
